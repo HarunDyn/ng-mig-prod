d40669e0c8a7322ca6953ed060f071ef3f43209f
Harun Dayan <harundayann@gmail.com>
2023-03-20 01:01:38 +0300

    add-count-product-to-badge-header

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
 import { MenuItem } from 'primeng/api';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-header',
@@ -7,37 +9,17 @@ import { MenuItem } from 'primeng/api';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
-  items!: MenuItem[];
+  countProduct = "0";
 
-  constructor() { }
-
-  ngOnInit() {
-    this.items = [
-      {
-        label: 'File',
-        items: [{
-          label: 'New',
-          icon: 'pi pi-fw pi-plus',
-          items: [
-            { label: 'Project' },
-            { label: 'Other' },
-          ]
-        },
-        { label: 'Open' },
-        { label: 'Quit' }
-        ]
-      },
-      {
-        label: 'Edit',
-        icon: 'pi pi-fw pi-pencil',
-        items: [
-          { label: 'Delete', icon: 'pi pi-fw pi-trash' },
-          { label: 'Refresh', icon: 'pi pi-fw pi-refresh' }
-        ]
-      }
-    ];
+  cartProductData$!: Observable<any>;
+  constructor(private store: Store<any>) {
+    this.cartProductData$ = this.store.select('cart');
   }
 
-
+  ngOnInit() {
+    this.cartProductData$.subscribe((value) => {
+      this.countProduct = (value.total).toString();
+    })
+  }
 
 }
